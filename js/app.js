bb35dbad01f70f744d5fc6706073eb6a98c34b99
BINGNAN, CHEN <59923237+r1991108@users.noreply.github.com>
2021-10-30 12:42:49 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,5 +7,20 @@ for (let i = 0; i < text.length; i++) {
 const lastWord = document.querySelector("#fourth");
 const animation = document.querySelector("div.animation");
 lastWord.addEventListener("animationend", () => {
-  animation.style = "transition: all 1s ease 0.2s; opacity: 0; pointer-events: none; transform: scale(1.5, 1.5);";
-})
+  animation.style =
+    "transition: all 1s ease 0.2s; opacity: 0; pointer-events: none; transform: scale(1.5, 1.5);";
+});
+
+let header = document.querySelector(".headerContainer");
+let nav = document.querySelector(".headerContainer nav");
+let nav_bk = nav.innerHTML;
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset != 0) {
+    nav.innerHTML = "";
+    header.style.boxShadow = "0 10px 10px -5px rgb(85, 85, 85)";
+    header.style.backgroundColor = "red";
+  } else {
+    nav.innerHTML = nav_bk;
+    header.style.boxShadow = "";
+  }
+});
